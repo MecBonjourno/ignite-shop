2a55a7d59511dedf4033120c43b8c60cf55c6fe1
Mecbonjourno <contatogzii@gmail.com>
2022-12-22 10:45:49 -0300

    fully functional ignite-shop

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -6,7 +6,6 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   const { priceId } = req.body
-  console.log(priceId)
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Should not be here.' })
@@ -16,7 +15,7 @@ export default async function handler(
     return res.status(400).json({ error: 'Should not be here.' })
   }
 
-  const successUrl = `${process.env.NEXT_URL}/success`
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
   const cancelUrl = `${process.env.NEXT_URL}/`
 
   const checkoutSession = await stripe.checkout.sessions.create({
